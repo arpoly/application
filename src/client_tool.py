2a160ff4d78ce037daa49f78c0f6aa9ed38d2193
arseniy.polyakov <arseniy.polyakov@NB-3292.local>
2023-07-04 18:14:01 +0300

    client error handle

--- src/client_tool.py
+++ src/client_tool.py
@@ -1,15 +1,27 @@
-import requests as r
-
+import logging
+import requests
 from src.utils.constants import VK_HOST
 
 
 class Client:
 
     @classmethod
-    def get(cls, path: str, params):
-        return r.get(url=VK_HOST + path, params=params)
+    def get(cls, path: str, params: dict = None):
+        try:
+            response = requests.get(url=VK_HOST + path, params=params)
+            response.raise_for_status()
+            return response
+        except requests.RequestException as error:
+            logging.error(f"An error occurred during GET request: {error}")
+            return None
 
     @classmethod
-    def post(cls, path: str, params):
-        return r.post(url=VK_HOST + path, params=params,
-                      headers={"Content-type": "application/json; charset=UTF-8"})
+    def post(cls, path: str, params: dict = None):
+        try:
+            headers = {"Content-type": "application/json; charset=UTF-8"}
+            response = requests.post(url=VK_HOST + path, params=params, headers=headers)
+            response.raise_for_status()
+            return response
+        except requests.RequestException as error:
+            logging.error(f"An error occurred during POST request: {error}")
+            return None
